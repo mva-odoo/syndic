5ef300405fb88fbc55432d57aa920a050831c261
mva-odoo <mva@odoo.com>
2015-10-05 16:27:56 +0200

    return value for default value

--- syndic_compta/models/compta.py
+++ syndic_compta/models/compta.py
@@ -2,6 +2,18 @@
 from openerp import models, fields, api, exceptions
 import datetime
 
+class SyndicComptaSetting(models.Model):
+    _name = 'syndic.compta.setting'
+
+    roulement_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (roulement)')
+    reserve_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (resrve)')
+    compte_rapporter = fields.Many2one('syndic.pcmn', 'Compte à reporter')
+    report_reserve_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de reserve à reporter')
+    report_roulement_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de roulement à reporter')
+    open_report_reserve_compte = fields.Many2one('syndic.product', 'Produit de fond de reserve à reporter pour reouverture')
+    open_report_roulement_compte = fields.Many2one('syndic.product', 'Produit de fond de roulement à reporter pour reouverture')
+    fournisseur_compte = fields.Many2one('syndic.pcmn', 'Compte fournisseur')
+
 class SyndicBuilding(models.Model):
     _inherit = 'syndic.building'
 
@@ -9,45 +21,61 @@ class SyndicBuilding(models.Model):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.roulement_product_id.id
+        return settings.roulement_product_id
 
     def _default_prod_reserve(self):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.reserve_product_id.id
+        return settings.roulement_product_id
 
     def _default_compte_rapporter(self):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.compte_rapporter.id
+        return settings.roulement_product_id
 
     def _default_report_reserve_compte(self):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.report_reserve_compte.id
+        return settings.roulement_product_id
 
     def _default_report_roulement_compte(self):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.report_roulement_compte.id
+        return settings.roulement_product_id
 
     def _default_open_report_reserve_compte(self):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.open_report_reserve_compte.id
+        return settings.roulement_product_id
 
     def _default_open_report_reserve_compte(self):
         settings = self.env['syndic.compta.setting'].search([])
         if len(settings) > 0:
             return settings.open_report_roulement_compte.id
+        return settings.roulement_product_id
 
 
-    roulement_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (roulement)', default=_default_prod_roulement)
-    reserve_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (resrve)', default=_default_prod_reserve)
-    compte_rapporter = fields.Many2one('syndic.pcmn', 'Compte à reporter', default=_default_compte_rapporter)
-    report_reserve_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de reserve à reporter', default=_default_report_reserve_compte)
-    report_roulement_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de roulement à reporter', default=_default_report_roulement_compte)
-    open_report_reserve_compte = fields.Many2one('syndic.product', 'Produit de fond de reserve à reporter pour reouverture', default=_default_open_report_reserve_compte)
-    open_report_roulement_compte = fields.Many2one('syndic.product', 'Produit de fond de roulement à reporter pour reouverture', default=_default_open_report_reserve_compte)
+    roulement_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (roulement)',
+                                           default=_default_prod_roulement)
+    reserve_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (resrve)',
+                                         default=_default_prod_reserve)
+    compte_rapporter = fields.Many2one('syndic.pcmn', 'Compte à reporter',
+                                       default=_default_compte_rapporter)
+    report_reserve_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de reserve à reporter',
+                                            default=_default_report_reserve_compte)
+    report_roulement_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de roulement à reporter',
+                                              default=_default_report_roulement_compte)
+    open_report_reserve_compte = fields.Many2one('syndic.product',
+                                                 'Produit de fond de reserve à reporter pour reouverture',
+                                                 default=_default_open_report_reserve_compte)
+    open_report_roulement_compte = fields.Many2one('syndic.product',
+                                                   'Produit de fond de roulement à reporter pour reouverture',
+                                                   default=_default_open_report_reserve_compte)
     current_exercice_id = fields.Many2one('syndic.exercice', 'Exercice courant')
     detail_ids = fields.Many2many('syndic.bilan.ligne', string='Lignes comptable')
 
@@ -67,15 +95,3 @@ class SyndicLot(models.Model):
     _inherit = 'syndic.lot'
 
     line_facture_ids = fields.One2many('syndic.facture.detail', 'lot_id', string="Lignes de facture")
-
-class SyndicComptaSetting(models.Model):
-    _name = 'syndic.compta.setting'
-
-    roulement_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (roulement)')
-    reserve_product_id = fields.Many2one('syndic.product', 'Produit d ouverture (resrve)')
-    compte_rapporter = fields.Many2one('syndic.pcmn', 'Compte à reporter')
-    report_reserve_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de reserve à reporter')
-    report_roulement_compte = fields.Many2one('syndic.pcmn', 'Compte de fond de roulement à reporter')
-    open_report_reserve_compte = fields.Many2one('syndic.product', 'Produit de fond de reserve à reporter pour reouverture')
-    open_report_roulement_compte = fields.Many2one('syndic.product', 'Produit de fond de roulement à reporter pour reouverture')
-    fournisseur_compte = fields.Many2one('syndic.pcmn', 'Compte fournisseur')
