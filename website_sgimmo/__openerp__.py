3066404652cd3d3054661d5d60a4622aa9ba772b
mva-odoo <mva@odoo.com>
2015-11-12 11:15:21 +0100

    correct , in openerp

--- website_sgimmo/__openerp__.py
+++ website_sgimmo/__openerp__.py
@@ -16,7 +16,7 @@ Site web SGImmo
         'pages/contact.xml',
         'pages/consultance.xml',
         # 'pages/footer.xml',
-        'views/res_users.xml'
+        'views/res_users.xml',
         'views/website_change.xml'
     ],
     'auto_install': False,
