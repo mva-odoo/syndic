bdc82a74fb8c9b5059e219da8b528d20b0064a72
mva-odoo <mva@odoo.com>
2015-12-03 18:59:44 +0100

    pdf viewer improvements

--- pdf_viewer/static/src/js/pdf_viewer.js
+++ pdf_viewer/static/src/js/pdf_viewer.js
@@ -4,13 +4,13 @@ openerp.pdf_viewer = function(instance, local) {
     var QWeb = instance.web.qweb;
 
 
+
     local.HomePage = instance.Widget.extend({
         template: "PDFViewer",
         start: function() {
             console.log("load pdf viewer");
         },
         init: function(parent, action) {
-            debugger;
             this._super(parent);
             this.report = action.context['report']
             this.url = '/report/pdf/'+action.context['report']+'/'+action.context['active_id'];
@@ -19,4 +19,48 @@ openerp.pdf_viewer = function(instance, local) {
 
 
     instance.web.client_actions.add('pdf_viewer.homepage', 'instance.pdf_viewer.HomePage');
+
+
 }
+
+
+odoo.define('sgimmo.custom_report', function (require) {
+    var ActionManager = require('web.ActionManager');
+    var core = require('web.core');
+    var crash_manager = require('web.crash_manager');
+    var framework = require('web.framework');
+    var session = require('web.session');
+    var pyeval = require('web.pyeval');
+
+     ActionManager.include({
+        ir_actions_report_xml: function(action, options) {
+            var self = this;
+            framework.blockUI();
+            action = _.clone(action);
+            var eval_contexts = ([session.user_context] || []).concat([action.context]);
+
+            _t =  core._t;
+            new_action = {
+                'tag': 'pdf_viewer.homepage',
+                'name': 'pdf_viewer',
+                'type': "ir.actions.client",
+            };
+
+            new_action.context = pyeval.eval('contexts',eval_contexts);
+            new_action.context['report'] = action['report_name']
+
+//            return self.ir_actions_client(new_action,options);
+
+            return $.Deferred(function (d) {
+                self.ir_actions_client(new_action,options).always(function () {
+                    framework.unblockUI();
+                    d.resolve();
+                });
+
+
+            });
+
+
+        }
+    });
+});
