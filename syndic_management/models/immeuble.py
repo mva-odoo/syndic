72343a7d91393e84ba2a6d9bffe5f0070fddc9c3
mva-odoo <mva@odoo.com>
2015-12-07 15:00:09 +0100

    FIX export fiche technique

--- syndic_management/models/immeuble.py
+++ syndic_management/models/immeuble.py
@@ -6,48 +6,10 @@ import base64
 
 
 class ExportFicheTech(models.TransientModel):
-    _name ='export.fiche.tech'
+    _name = 'export.fiche.tech'
 
-    xls_file = fields.Binary("cliquez ici", readonly=True, default='_get_export_fiche_technique')
-    datas_fname = fields.Char("Fichier", readonly=True, default='export.xls')
-
-    @api.model
-    def _get_export_fiche_technique(self):
-        if self._context:
-            active_id = self._context.get('active_id')
-            xls = Workbook()
-            # sheet 1 creation
-            feuil1 = xls.add_sheet('sheet 1')
-
-            # add header
-            row = 0
-            feuil1.write(row, 0, 'Nom du fournisseur')
-            feuil1.write(row, 1, 'Metier')
-            feuil1.write(row, 2, 'Adresse')
-            feuil1.write(row, 3, 'Ville')
-            feuil1.write(row, 4, 'Code Postal')
-            feuil1.write(row, 5, 'Pays')
-            feuil1.write(row, 6, 'Telephone')
-            feuil1.write(row, 7, 'Email')
-
-            for fournisseur_id in self.env['syndic.building'].search([('id', '=', active_id)]).supplier_ids:
-                row += 1
-                metier_ids = ','.join([job_id.name for job_id in fournisseur_id.job_ids])
-
-                feuil1.write(row, 0, fournisseur_id.name)
-                feuil1.write(row, 1, metier_ids)
-                feuil1.write(row, 2, fournisseur_id.street)
-                feuil1.write(row, 3, fournisseur_id.city_id.name)
-                feuil1.write(row, 4, fournisseur_id.zip)
-                feuil1.write(row, 5, fournisseur_id.country_id.name)
-                feuil1.write(row, 6, fournisseur_id.phone)
-                feuil1.write(row, 7, fournisseur_id.email)
-
-            output = StringIO.StringIO()
-            xls.save(output)
-            encode_text = base64.encodestring(output.getvalue())
-
-            return encode_text
+    xls_file = fields.Binary("Fichier", readonly=True)
+    datas_fname = fields.Char("Nom du Fichier", readonly=True, default='')
 
 
 class Building(models.Model):
@@ -74,8 +36,46 @@ class Building(models.Model):
     active = fields.Boolean(default=True)
 
     @api.multi
+    def compute_xls(self):
+        xls = Workbook()
+        feuil1 = xls.add_sheet('sheet 1')
+
+        row = 0
+        feuil1.write(row, 0, 'Nom du fournisseur')
+        feuil1.write(row, 1, 'Metier')
+        feuil1.write(row, 2, 'Adresse')
+        feuil1.write(row, 3, 'Ville')
+        feuil1.write(row, 4, 'Code Postal')
+        feuil1.write(row, 5, 'Pays')
+        feuil1.write(row, 6, 'Telephone')
+        feuil1.write(row, 7, 'Email')
+
+        for fournisseur_id in self.supplier_ids:
+            row += 1
+            metier_ids = ','.join([job_id.name for job_id in fournisseur_id.job_ids])
+
+            feuil1.write(row, 0, fournisseur_id.name)
+            feuil1.write(row, 1, metier_ids)
+            feuil1.write(row, 2, fournisseur_id.street)
+            feuil1.write(row, 3, fournisseur_id.city_id.name)
+            feuil1.write(row, 4, fournisseur_id.zip)
+            feuil1.write(row, 5, fournisseur_id.country_id.name)
+            feuil1.write(row, 6, fournisseur_id.phone)
+            feuil1.write(row, 7, fournisseur_id.email)
+
+        output = StringIO.StringIO()
+        xls.save(output)
+        return self.env['export.fiche.tech'].create({
+            'xls_file': base64.encodestring(output.getvalue()),
+            'datas_fname': 'export_%s.xls' % self.name,
+        })
+
+    @api.multi
     def go_export_model(self):
+        res_id = self.compute_xls()
+
         return {
+            'res_id': res_id.id,
             'name': 'Export ficher technique',
             'view_type': 'form',
             'view_mode': 'form',
