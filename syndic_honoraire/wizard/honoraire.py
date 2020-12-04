79e1ce969c781603e2d750a058c9be78b4852ce9
mva <mva@odoo.com>
2020-12-04 21:58:49 +0100

    invoice

--- syndic_honoraire/wizard/honoraire.py
+++ syndic_honoraire/wizard/honoraire.py
@@ -11,66 +11,84 @@ class SuiviFacture(models.TransientModel):
         ('2', '2'),
         ('3', '3'),
         ('4', '4'),
-    ], string='Trimestre', required=True)
-    year_id = fields.Many2one('syndic.honoraire.year', u'Année', required=True)
-    date = fields.Date('Date', default=lambda *a: fields.date.today(), required=True)
+    ], string='Trimestre')
+    year_id = fields.Many2one('syndic.honoraire.year', u'Année')
+    date = fields.Date('Date', default=lambda *a: fields.date.today())
+    amount = fields.Float('Montant')
+    description = fields.Char('Description')
+    is_fix = fields.Boolean('Frais fix')
 
     def invoice_generate(self):
         self.ensure_one()
         invoices = self.env['account.move']
 
         active_buildings = self.env['syndic.building'].browse(self._context.get('active_ids'))
-        buildings = active_buildings & self.year_id.mapped('honoraire_ids.building_id')
-
+        if not self.is_fix:
+            buildings = active_buildings & self.year_id.mapped('honoraire_ids.building_id')
+        else:
+            buildings = active_buildings
 
         for building in buildings:
-            frais = self.env['syndic.honoraire'].search([
-                ('year_id', '=', self.year_id.id),
-                ('building_id', '=', building.id)
-            ], limit=1)
+            if not self.is_fix:
+                frais = self.env['syndic.honoraire'].search([
+                    ('year_id', '=', self.year_id.id),
+                    ('building_id', '=', building.id)
+                ], limit=1)
 
-            frais_admin_vals = {
-                'name': 'Frais administratifs %s - %s' % (self.year_id.name, self.trimestre),
-                'price_unit': frais.frais_admin,
-                'quantity': 1,
-                'account_id': self.env.ref('l10n_be.1_a7010').id,
+                frais_admin_vals = {
+                    'name': 'Frais administratifs %s - %s' % (self.year_id.name, self.trimestre),
+                    'price_unit': frais.frais_admin,
+                    'quantity': 1,
+                    'account_id': self.env.ref('l10n_be.1_a7010').id,
 
-            }
+                }
 
-            honoraire_vals = {
-                'name': 'Honoraires %s - %s' % (self.year_id.name, self.trimestre),
-                'price_unit': frais.honoraire,
-                'quantity': 1,
-                'account_id': self.env.ref('l10n_be.1_a7010').id,
+                honoraire_vals = {
+                    'name': 'Honoraires %s - %s' % (self.year_id.name, self.trimestre),
+                    'price_unit': frais.honoraire,
+                    'quantity': 1,
+                    'account_id': self.env.ref('l10n_be.1_a7010').id,
 
-            }
-            
-            if building.is_merge:
-                invoices |= self.env['account.move'].create({
-                    'type': 'out_invoice',
-                    'date': self.date,
-                    'building_id': building.id,
-                    'partner_id': building.user_id.partner_id.id,
-                    'invoice_line_ids': [(0, 0, frais_admin_vals), (0, 0, honoraire_vals)]
-                })
+                }
 
+                if building.is_merge:
+                    invoices |= self.env['account.move'].create({
+                        'type': 'out_invoice',
+                        'date': self.date,
+                        'building_id': building.id,
+                        'partner_id': building.partner_id.id,
+                        'invoice_line_ids': [(0, 0, frais_admin_vals), (0, 0, honoraire_vals)]
+                    })
+
+                else:
+                    invoices |= self.env['account.move'].create({
+                        'type': 'out_invoice',
+                        'date': self.date,
+                        'building_id': building.id,
+                        'partner_id': building.partner_id.id,
+                        'invoice_line_ids': [(0, 0, frais_admin_vals)]
+                    })
+
+                    invoices |= self.env['account.move'].create({
+                        'type': 'out_invoice',
+                        'date': self.date,
+                        'building_id': building.id,
+                        'partner_id': building.partner_id.id,
+                        'invoice_line_ids': [(0, 0, honoraire_vals)]
+                    })
             else:
                 invoices |= self.env['account.move'].create({
                     'type': 'out_invoice',
                     'date': self.date,
                     'building_id': building.id,
-                    'partner_id': building.user_id.partner_id.id,
-                    'invoice_line_ids': [(0, 0, frais_admin_vals)]
-                })
-
-                invoices |= self.env['account.move'].create({
-                    'type': 'out_invoice',
-                    'date': self.date,
-                    'building_id': building.id,
-                    'partner_id': building.user_id.partner_id.id,
-                    'invoice_line_ids': [(0, 0, honoraire_vals)]
-                })
-
+                    'partner_id': building.partner_id.id,
+                    'invoice_line_ids': [(0, 0, {
+                        'name': self.description,
+                        'price_unit': self.amount,
+                        'quantity': 1,
+                        'account_id': self.env.ref('l10n_be.1_a7010').id,
+                    })]
+                    })
         action = self.env.ref('account.action_move_out_invoice_type').read()[0]
         action['domain'] = [('id', 'in', invoices.ids)]
 
