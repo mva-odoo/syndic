b62ee14fe312d05a6a2f4c3310b7f078aa99fe22
mva-odoo <mva@odoo.com>
2020-12-08 22:07:31 +0100

    [SOGIS]

---
+++ syndic_tools/contorllers/main.py
@@ -0,0 +1,27 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.syndic_tools.syndic_tools import SyndicTools
+
+import base64
+import json
+
+
+class ReportController(http.Controller):
+    @http.route([
+        '/multi_pdf_downloads/<docids>',
+    ], type='http', auth='user', website=True)
+    def get_pdf(self, docids=None):
+        pdf_merge = False
+        if docids:
+            docids = [int(i) for i in docids.split(',')]
+            pdf = []
+            pdf_len = 0
+            for invoice in request.env['sogis.invoice'].browse(docids).filtered(lambda s:s.watermark_pdf):
+                pdf_data = base64.b64decode(invoice.watermark_pdf)
+                pdf.append(pdf_data)
+                pdf_len += len(pdf_data)
+
+                pdf_merge = SyndicTools().merge_pdf(pdf)
+            if pdf_merge:
+                pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
+                return request.make_response(pdf_merge, headers=pdfhttpheaders)
